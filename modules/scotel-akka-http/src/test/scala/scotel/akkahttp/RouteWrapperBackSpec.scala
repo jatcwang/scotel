3a4ba873e302587925505bfbb0aaf4197c1296b1
Jacob Wang <jatcwang@gmail.com>
2021-03-08 23:44:49 +0000

    delme

---
+++ modules/scotel-akka-http/src/test/scala/scotel/akkahttp/RouteWrapperBackSpec.scala
@@ -0,0 +1,53 @@
+package scotel.akkahttp
+
+import io.opentelemetry.sdk.trace.`export`.SpanExporter
+import munit.FunSuite
+import scotel.testutils.{
+  setupTraceProviderWithNewThreadPool,
+  InMemorySpanExporter,
+}
+import RouteWrapper.tracedRoute
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.wordspec.AnyWordSpec
+
+import scala.concurrent.Future
+
+class RouteWrapperBackSpec
+    extends AnyWordSpec
+    with ScalatestRouteTest
+    with BeforeAndAfterEach {
+
+  val spanExporter: InMemorySpanExporter = InMemorySpanExporter()
+  val (ec, tracer) = setupTraceProviderWithNewThreadPool(spanExporter)
+
+  "Records and finishes span for a route" in {}
+  override def afterEach(): Unit = {
+    spanExporter.reset()
+    super.afterEach()
+  }
+
+  override def afterAll(): Unit = {
+    super.afterAll()
+  }
+
+  lazy val route = tracedRoute(tracer = tracer, ec = ec, actorSystem = system) {
+    import akka.http.scaladsl.server.Directives._
+
+    get {
+      path("hello" / "world") {
+        onSuccess(
+          Future { 1 + 1 },
+        )(_ => complete(StatusCodes.OK))
+      }
+    }
+  }
+
+  override def createActorSystem(): ActorSystem = {
+    ActorSystem("RouteWrapperSpec", defaultExecutionContext = Some(ec))
+  }
+
+}
